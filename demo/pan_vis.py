a672fe9b77497ee763b403c5c8ebd3f4fb63a23e
sahan-liyanaarachchi <47945053+sahan-liyanaarachchi@users.noreply.github.com>
2020-04-21 16:42:43 +0530

    Update pan_vis.py

--- demo/pan_vis.py
+++ demo/pan_vis.py
@@ -4,11 +4,61 @@ import multiprocessing as mp
 from collections import deque
 import cv2
 import torch
+import argparse
+import glob
+import os
 
 from detectron2.data import MetadataCatalog
 from detectron2.engine.defaults import DefaultPredictor
 from detectron2.utils.video_visualizer import VideoVisualizer
 from detectron2.utils.visualizer import ColorMode, Visualizer
+from detectron2.config import get_cfg
+from detectron2.data.detection_utils import read_image
+from detectron2.utils.logger import setup_logger
+
+def setup_cfg(args):
+    # load config from file and command-line arguments
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    # Set score_threshold for builtin models
+    cfg.MODEL.RETINANET.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = args.confidence_threshold
+    cfg.freeze()
+    return cfg
+def get_parser():
+    parser = argparse.ArgumentParser(description="Detectron2 demo for builtin models")
+    parser.add_argument(
+        "--config-file",
+        default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
+        metavar="FILE",
+        help="path to config file",
+    )
+    parser.add_argument(
+        "--output",
+        help="A file or directory to save output visualizations. "
+        "If not given, will show output in an OpenCV window.",
+    )
+    parser.add_argument(
+        "--input",
+        help="A file or directory to save output visualizations. "
+        "If not given, will show output in an OpenCV window.",
+    )
+
+    parser.add_argument(
+        "--confidence-threshold",
+        type=float,
+        default=0.5,
+        help="Minimum score for instance predictions to be shown",
+    )
+    parser.add_argument(
+        "--opts",
+        help="Modify config options using the command-line 'KEY VALUE' pairs",
+        default=[],
+        nargs=argparse.REMAINDER,
+    )
+    return parser
 
 
 class VisualizationDemo(object):
@@ -56,6 +106,23 @@ class VisualizationDemo(object):
             vis_output_ins= visualizer.draw_pan_ins_predictions(
                 panoptic_seg.to(self.cpu_device), segments_info
             )
-            
+        return vis_output, vis_output_sem, vis_output_ins
         
-        return predictions, vis_output
+if __name__ == "__main__":    
+    args = get_parser().parse_args()
+    setup_logger(name="fvcore")
+    logger = setup_logger()
+    logger.info("Arguments: " + str(args))
+
+    cfg = setup_cfg(args)
+
+    demo = VisualizationDemo(cfg)
+    img_path = args.input # path to image
+    img_list = os.listdir(img_path)
+    save_path = args.output
+    for im in img_list:
+        img = read_image(os.join(img_path,im), format="BGR")
+        pan, sem, ins = demo.run_on_image(img)
+        pan.save(os.join(save_path,'{}_pan.png'.format(im[:-4])))
+        sem.save(os.join(save_path,'{}_sem.png'.format(im[:-4])))
+        ins.save(os.join(save_path,'{}_ins.png'.format(im[:-4])))
