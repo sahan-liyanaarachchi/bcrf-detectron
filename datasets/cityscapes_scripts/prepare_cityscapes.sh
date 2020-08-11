f0f71f0c15703d4dabb932e152c45cf78a39c50d
sahan-liyanaarachchi <47945053+sahan-liyanaarachchi@users.noreply.github.com>
2020-08-11 23:58:02 +0530

    Update prepare_cityscapes.sh

--- datasets/cityscapes_scripts/prepare_cityscapes.sh
+++ datasets/cityscapes_scripts/prepare_cityscapes.sh
@@ -1,11 +1,11 @@
-CITYSCAPES_PATH="/home/sahanchamara99/data/cityscapes"
+CITYSCAPES_PATH=$1
 
-mkdir annotations
+mkdir $CITYSCAPES_PATH/annotations
 python pan_city2coco.py $CITYSCAPES_PATH
-python ins_city2coco.py --datadir $CITYSCAPES_PATH --outdir annotations
-mkdir train
-mkdir val
-cp $CITYSCAPES_PATH/leftImg8bit/train/*/*.png train/
-cp $CITYSCAPES_PATH/leftImg8bit/val/*/*.png val/
-python prepare_panoptic_fpn.py
+python ins_city2coco.py --datadir $CITYSCAPES_PATH --outdir $CITYSCAPES_PATH/annotations
+mkdir $CITYSCAPES_PATH/train
+mkdir $CITYSCAPES_PATH/val
+cp $CITYSCAPES_PATH/leftImg8bit/train/*/*.png $CITYSCAPES_PATH/train/
+cp $CITYSCAPES_PATH/leftImg8bit/val/*/*.png $CITYSCAPES_PATH/val/
+python prepare_panoptic_fpn.py $CITYSCAPES_PATH
 
